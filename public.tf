b617b98a387dfe0de3fbf8bb8322a11d383bb84c
jairowmd <jairowmd@hotmail.com>
2024-03-25 22:20:29 -0300

    subindo subnets publicas

---
+++ public.tf
@@ -0,0 +1,26 @@
+
+# criando subnet availability_zone = "us-east-1a"
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "cursodevops-subnet-1a"
+  }
+}
+
+
+
+# criando subnet availability_zone = "us-east-1b"
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "cursodevops-subnet-1b"
+  }
+}
