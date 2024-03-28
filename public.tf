66305f87c7bcb14f03857c1befa632712d5245f9
jairowmd <jairowmd@hotmail.com>
2024-03-28 19:56:31 -0300

    adicionado region e modificado public como variaveis

--- public.tf
+++ public.tf
@@ -1,14 +1,16 @@
 
 # criando subnet availability_zone = "us-east-1a"
 resource "aws_subnet" "eks_subnet_public_1a" {
-  vpc_id                  = aws_vpc.eks_vpc.id
+  vpc_id = aws_vpc.eks_vpc.id
   # informado no arquivo var o cidr de referencia para o calculo
-  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
-  availability_zone       = "us-east-1a"
+  cidr_block = cidrsubnet(var.cidr_block, 8, 1)
+  # variavel em region.tf
+  availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "cursodevops-subnet-1a"
+    Name                    = "cursodevops-subnet-1a",
+    "kubernets.io/role/elb" = 1
   }
 }
 
@@ -16,13 +18,15 @@ resource "aws_subnet" "eks_subnet_public_1a" {
 
 # criando subnet availability_zone = "us-east-1b"
 resource "aws_subnet" "eks_subnet_public_1b" {
-  vpc_id                  = aws_vpc.eks_vpc.id
+  vpc_id = aws_vpc.eks_vpc.id
   # informado no arquivo var o cidr de referencia para o calculo
-  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
-  availability_zone       = "us-east-1b"
+  cidr_block = cidrsubnet(var.cidr_block, 8, 2)
+  # variavel em region.tf
+  availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "cursodevops-subnet-1b"
+    Name                    = "cursodevops-subnet-1b"
+    "kubernets.io/role/elb" = 1
   }
 }
