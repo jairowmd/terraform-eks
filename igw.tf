9f0d4bdcdf867b99d3c5bda34d3e21f500ded633
jairowmd <jairowmd@hotmail.com>
2024-03-28 23:09:07 -0300

    criando Internet Gateway - igw - e Route Table - rtb

---
+++ igw.tf
@@ -0,0 +1,44 @@
+# criando recurso de internet gateway
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+ # estou fazendo um merge da tag do recurso com as definidas no locals.tf
+  tags = merge(
+
+    local.tags,
+
+    {
+
+      Name = "${var.project_name}-igw"
+
+    }
+
+  )
+
+
+}
+
+# criando a route_table 
+
+resource "aws_route_table" "eks_public_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  # estou fazendo um merge da tag do recurso com as definidas no locals.tf
+  tags = merge(
+
+    local.tags,
+
+    {
+
+      Name = "${var.project_name}-public-rtb"
+
+    }
+
+  )
+
+}
