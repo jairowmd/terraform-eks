3920e24153a309112e44f7dab57eea753ecd85c3
jairowmd <jairowmd@hotmail.com>
2024-04-04 22:28:18 -0300

    config deploy helm

--- provider.tf
+++ provider.tf
@@ -12,6 +12,10 @@ terraform {
       version = "2.27.0"
     }
 
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.13.0"
+    }
 
   }
 
@@ -36,5 +40,19 @@ provider "kubernetes" {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
     command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+
+
+    host                   = module.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+      command     = "aws"
+    }
   }
 }
